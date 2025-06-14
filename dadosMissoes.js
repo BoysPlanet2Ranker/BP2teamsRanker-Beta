06c3e6da346ec5928fde901c4d1a182780faba45
BoysPlanet2Ranker <seongjair@gmail.com>
2025-06-14 16:53:20 -0300

    Add files via upload

---
+++ dadosMissoes.js
@@ -0,0 +1,23 @@
+const dadosMissoes = {
+    // Dados para a Missão 1
+    missao1: {
+        teams: {
+            team1: {
+                nome: "Hot Sauce",
+                traineeIds: [1, 8, 15, 22, 29, 36, 43] // Exemplo: IDs dos trainees deste time
+            },
+            team2: {
+                nome: "Danger",
+                traineeIds: [2, 9, 16, 23, 30, 37, 44] // Exemplo
+            },
+            team3: {
+                nome: "Burn It Up",
+                traineeIds: [3, 10, 17, 24, 31, 38, 45] // Exemplo
+            }
+        }
+    },
+    // Você pode adicionar missao2, missao3, etc. aqui no futuro
+    missao2: {
+        // ...
+    }
+};
